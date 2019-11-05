e541e07caa8150e14e622c8a2a67f246a0312ebc
dephiloper <s0554213@htw-berlin.de>
2019-11-05 14:40:46 +0100

    finished window creation 'hello window'

---
+++ ogl_getting_started/main.cpp
@@ -0,0 +1,82 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow *window);
+
+// settings
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
+
+void processInput(GLFWwindow *window)
+{
+    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    // make sure the viewport matches the new window dimensions; note that width and
+    // height will be significantly larger than specified on retina displays.
+    glViewport(0, 0, width, height);
+}
+
+int main()
+{
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); // use only subset of core functionality
+
+    // glfw window creation, check if window is successfully created, otherwise error
+    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", nullptr, nullptr);
+    if (window == nullptr)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    // glad: load all OpenGL function pointers, check if glad is successfully included
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    // render loop
+    // -----------
+    while (!glfwWindowShouldClose(window))
+    {
+        // input
+        processInput(window);
+
+        // render
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f); // state-setting
+        glClear(GL_COLOR_BUFFER_BIT); // state-using (uses current state to retrieve color)
+
+        // double buffer
+        /*
+         * When an application draws in a single buffer the resulting image might display flickering issues.
+         * This is because the resulting output image is not drawn in an instant, but drawn pixel by pixel and
+         * usually from left to right and top to bottom. Because this image is not displayed at an instant to
+         * the user while still being rendered to, the result may contain artifacts. To circumvent these issues,
+         * windowing applications apply a double buffer for rendering. The front buffer contains the final output
+         * image that is shown at the screen, while all the rendering commands draw to the back buffer. As soon
+         * as all the rendering commands are finished we swap the back buffer to the front buffer so the image
+         * is instantly displayed to the user, removing all the aforementioned artifacts.
+         */
+
+        // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    // glfw: terminate, clearing all previously allocated GLFW resources.
+    glfwTerminate();
+    return 0;
+}

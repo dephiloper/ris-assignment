73be50e1156ec4dd5aa5dd65096517fe2ef01bb7
dephiloper <s0554213@htw-berlin.de>
2019-11-07 01:33:30 +0100

    shaders and file loading function added
    triangle visible

--- ogl_getting_started/main.cpp
+++ ogl_getting_started/main.cpp
@@ -1,14 +1,26 @@
+#pragma clang diagnostic push
+#pragma ide diagnostic ignored "cert-err58-cpp"
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
 #include <iostream>
+#include <fstream>
+#include <sstream>
+
+const std::string PROJECT_PATH = "/home/phil/Development/realtime-interactive-systems/ogl_getting_started/";
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow *window);
 
-// settings
-const unsigned int SCR_WIDTH = 800;
-const unsigned int SCR_HEIGHT = 600;
+/**
+ * https://stackoverflow.com/a/2602258
+ */
+std::string readFile(const std::string& filepath) {
+    std::ifstream t(filepath);
+    std::stringstream buffer;
+    buffer << t.rdbuf();
+    return buffer.str();
+}
 
 void processInput(GLFWwindow *window)
 {
@@ -31,7 +43,7 @@ int main()
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); // use only subset of core functionality
 
     // glfw window creation, check if window is successfully created, otherwise error
-    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", nullptr, nullptr);
+    GLFWwindow* window = glfwCreateWindow(800, 600, "OGL_GETTING_STARTED", nullptr, nullptr);
     if (window == nullptr)
     {
         std::cout << "Failed to create GLFW window" << std::endl;
@@ -47,6 +59,141 @@ int main()
         std::cout << "Failed to initialize GLAD" << std::endl;
         return -1;
     }
+
+    // render triangle
+    // -------------------------------------------------------------------
+
+    // create vertex shader
+    // -------------------------------------------------------------------
+    unsigned int vertexShader;
+    vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    if (vertexShader == 0) std::cout << "Error occurred while processing vertex shader." << std::endl;
+
+    // 1: shader object, 2: how many shader strings, 3: shader source code
+    std::string vertexShaderSource = readFile(PROJECT_PATH + "shaders/vertex.glsl");
+    const GLchar* sourcePtr = vertexShaderSource.c_str(); // https://stackoverflow.com/a/30804288
+    glShaderSource(vertexShader, 1, &sourcePtr, nullptr);
+    glCompileShader(vertexShader);
+
+    // check compilation for errors
+    int success;
+    char infoLog[512];
+
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+    if(!success)
+    {
+        glGetShaderInfoLog(vertexShader, 512, nullptr, infoLog);
+        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+
+    // create fragment shader
+    // -------------------------------------------------------------------
+    unsigned int fragmentShader;
+    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    if (fragmentShader == 0) std::cout << "Error occurred while processing fragment shader." << std::endl;
+
+    // 1: shader object, 2: how many shader strings, 3: shader source code
+    std::string fragmentShaderSource = readFile(PROJECT_PATH + "shaders/fragment.glsl");
+    sourcePtr = fragmentShaderSource.c_str();
+    glShaderSource(fragmentShader, 1, &sourcePtr, nullptr);
+    glCompileShader(fragmentShader);
+
+    // check compilation for errors
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+    if(!success)
+    {
+        glGetShaderInfoLog(fragmentShader, 512, nullptr, infoLog);
+        std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+
+    // linking shaders into shader program
+    // -------------------------------------------------------------------
+    unsigned int shaderProgram;
+    shaderProgram = glCreateProgram();
+    if (shaderProgram == 0) std::cout << "Error occurred while creating shader program." << std::endl;
+
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+    if(!success)
+    {
+        glGetProgramInfoLog(shaderProgram, 512, nullptr, infoLog);
+        std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+    }
+
+    glUseProgram(shaderProgram);
+    // after creating the program we delete the shaders
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    // Attributes of glVertexAttribPointer
+    /* The first parameter specifies which vertex attribute we want to configure. Remember that we specified the
+     * location of the position vertex attribute in the vertex shader with layout (location = 0). This sets the
+     * location of the vertex attribute to 0 and since we want to pass data to this vertex attribute, we pass in 0.
+     *
+     * The next argument specifies the size of the vertex attribute. The vertex attribute is a vec3 so it is composed
+     * of 3 values.
+     *
+     * The third argument specifies the type of the data which is GL_FLOAT (a vec* in GLSL consists of floating
+     * point values).
+     *
+     * The next argument specifies if we want the data to be normalized. If we're inputting integer data types
+     * (int, byte) and we've set this to GL_TRUE, the integer data is normalized to 0 (or -1 for signed data)
+     * and 1 when converted to float. This is not relevant for us so we'll leave this at GL_FALSE.
+     *
+     * The fifth argument is known as the stride and tells us the space between consecutive vertex attributes.
+     * Since the next set of position data is located exactly 3 times the size of a float away we specify that
+     * value as the stride. Note that since we know that the array is tightly packed (there is no space between
+     * the next vertex attribute value) we could've also specified the stride as 0 to let OpenGL determine the
+     * stride (this only works when values are tightly packed). Whenever we have more vertex attributes we have to
+     * carefully define the spacing between each vertex attribute but we'll get to see more examples of that later on.
+     *
+     * The last parameter is of type void* and thus requires that weird cast. This is the offset of where the position data
+     * begins in the buffer. Since the position data is at the start of the data array this value is just 0. We will
+     * explore this parameter in more detail later on
+    */
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+
+    glEnableVertexAttribArray(0);
+
+    // vertices of an triangle
+    float vertices[] = {
+            -.5f, -.5f,  .0f,
+            .5f, -.5f,  .0f,
+            .0f,  .5f,  .0f
+    };
+
+
+    unsigned int VBO, VAO; // create unique id
+    glGenBuffers(1, &VBO); // create buffer object to corresponding id
+    glGenVertexArrays(1, &VAO); // create vertex array object
+
+    // 1. bind buffers and vertex array object
+    // -------------------------------------------------------------------
+    glBindBuffer(GL_ARRAY_BUFFER, VBO); // bind created buffer to buffer type GL_ARRAY_BUFFER (type of vertex buffer)
+    // after binding, every buffer calls on GL_ARRAY_BUFFER will be used to configure VBO
+    glBindVertexArray(VAO);
+
+    // 2. copy vertices array in a buffer for opengl to use
+    // copies vertex data into buffer memory
+    // 0: type of buffer we want copy data into
+    // 1: size of the data
+    // 2: the data
+    // 3: specifies how the graphics card should manage the given data
+    // 3.1: GL_STATIC_DRAW (data does not change or very rarely)
+    // 3.2: GL_DYNAMIC_DRAW (data changes a lot)
+    // 3.3: GL_STREAM_DRAW (data changes on every draw) -> 3.2 & 3.3 data is placed in memory for faster drawing
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    // 3. set vertex attribute pointers (need explanation)
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)nullptr);
+    glEnableVertexAttribArray(0);
+
+    // unbind buffer and vertex array so VAO calls don't accidentally modify VAO, but it's not necessary
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(VAO);
 
     // render loop
     // -----------
@@ -71,12 +218,22 @@ int main()
          * is instantly displayed to the user, removing all the aforementioned artifacts.
          */
 
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        // glBindVertexArray(0); // no need to unbind it every time
+
         // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
 
+    // optional: de-allocate all resources
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+
     // glfw: terminate, clearing all previously allocated GLFW resources.
     glfwTerminate();
     return 0;
 }
+#pragma clang diagnostic pop
